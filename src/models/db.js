d3890b758e51eecc4ee21c5b6ab51c1a88c5419d
Adriano <adriano.rodrigues.junior01@gmail.com>
2023-12-16 16:24:51 -0300

    tentativa3

--- src/models/db.js
+++ src/models/db.js
@@ -6,4 +6,13 @@ const sequelize = new Sequelize('gearserp', 'gearserp_user', 'ZNtA5wHsdL2v7UBvHu
     port: '5432',
 });
 
+// Testando a conexão
+sequelize.authenticate()
+    .then(() => {
+        console.log('Conexão com o Banco de Dados estabelecida com sucesso.');
+    })
+    .catch(err => {
+        console.error('Não foi possível conectar ao banco de dados:', err);
+    });
+
 module.exports = sequelize;
