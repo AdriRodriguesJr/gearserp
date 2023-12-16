e2a1fd38d3093539fe5c408ca7a7dd043595bab5
Adriano <adriano.rodrigues.junior01@gmail.com>
2023-12-16 16:20:21 -0300

    tentativa2

--- src/models/db.js
+++ src/models/db.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('Gearserp_db', 'gearserp_user', 'ZNtA5wHsdL2v7UBvHutsJHeUjsBK4JMq', {
+const sequelize = new Sequelize('gearserp', 'gearserp_user', 'ZNtA5wHsdL2v7UBvHutsJHeUjsBK4JMq', {
     host: 'dpg-cln9frhll56s73feckq0-a',
     dialect: 'postgres',
     port: '5432',
